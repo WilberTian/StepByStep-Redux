0701e8e55f275583db86db88d8fb3008172b972e
WilberTian <tianli917@hotmail.com>
2016-04-12 20:55:01 +0800

    add demo code of reducer

---
+++ demo/02.reducer.js
@@ -0,0 +1,41 @@
+const initialState = {
+    taskCategory: 'All',
+    tasks: []
+};
+
+function taskApp(state = initialState, action) {
+    switch (action.type) {
+        case 'SELECT_CATEGORY':
+            return Object.assign({}, state, {
+                taskCategory: action.category
+            });
+            
+        case 'ADD_TASK':
+            return Object.assign({}, state, {
+                tasks: [...state.tasks, {
+                    name: action.name,
+                    category: action.category
+                }]
+            });
+            
+        default:
+            return state;
+    }
+}
+
+
+let newState = taskApp(initialState, {
+    type: 'ADD_TASK',
+    name: 'Write a blog',
+    category: 'Writing'
+});
+console.log(newState);
+
+
+newState = taskApp(newState, {
+    type: 'ADD_TASK',
+    name: 'Read ES6 spec',
+    category: 'Reading'
+});
+console.log(newState);
+
